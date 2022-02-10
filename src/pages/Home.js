b6e515965f5f1416033f3191bed95167b1c1fa4d
Jym-ship-it <fjymcyruz@gmail.com>
2022-02-10 15:13:36 +0800

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
+import Nappbar from '../component/appbar'
 
 export default function Home() {
+    const style = {
+
+    }
     return (
         <Box>
-            <Typography>Hello WOrld</Typography>
+            <Box>
+            <Nappbar/>
+            </Box>
+            <Box>
+                hello
+            </Box>
         </Box>
     )
 }
